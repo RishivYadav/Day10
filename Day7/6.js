a27ee106ecebd676cbcec6b8bb2cc58b3ab06eb9
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-10 15:46:12 +0530

    day ten

---
+++ Day7/6.js
@@ -0,0 +1,8 @@
+let i = 0;
+let pi = 3.14;
+let active = true;
+let list = [];
+let ref = {};
+console.log(active);
+console.log(list);
+console.log(ref);
---
+++ Day8(fun)/2.js
@@ -0,0 +1,15 @@
+// Annonymous function
+// Assigning a function to a variable!
+// We can call annonymous function
+let sumfn = function (n1, n2) {
+    let output = n1 + n2;
+    return output;
+  };
+  
+  // Arrow Function => / ->
+  // Assigning the functio to a variable
+  let sumfn1 = (n1, n2) => {
+    let output = n1 + n2;
+    return output;
+  };
+  
